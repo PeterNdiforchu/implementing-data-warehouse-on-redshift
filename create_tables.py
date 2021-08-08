32a701fdc900c55de7aa6adf6b8bbc8971baf168
Peter Ndiforchu <76578061+PeterNdiforchu@users.noreply.github.com>
2021-08-08 08:31:24 -0300

    Update create_tables.py

--- create_tables.py
+++ create_tables.py
@@ -3,24 +3,27 @@ import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
 def drop_tables(cur, conn):
-    
-"""This function parses through the drop tables query defined in the sql_queries.py script and drops tables"""
+    """
+    This function parses through the drop tables query defined in the sql_queries.py script and drops tables
+    """
 
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 def create_tables(cur, conn):
-    
-"""This function parses through the create tables queries defined in the sql_queries.py script and creates tables"""
+     """
+     This function parses through the create tables queries defined in the sql_queries.py script and creates tables
+     """
 
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 def main():
-    
-"""This function executes the parameters for connecting to the redshift cluster as defined in the dwh.cfg script"""
+    """
+    This function executes the parameters for connecting to the redshift cluster as defined in the dwh.cfg script
+    """
 
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
